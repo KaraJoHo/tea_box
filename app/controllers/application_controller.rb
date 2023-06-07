5f1691c3869c944a2ed10edb5ea5f6bedb3bdfdb
KaraJH <karas.jones@yahoo.com>
2023-06-07 15:05:11 -0600

    feat: Error serializer and applicaiton controller rescues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActionController::ParameterMissing, with: :missing_parameter_response
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_reponse
+
+  def render_not_found_response(exception)
+    render json: ErrorSerializer.new(exception).bad_request, status: 404
+  end
+
+  def missing_parameter_response(exception)
+    render json: ErrorSerializer.new(exception).bad_request, status: 400
+  end
+
+  def invalid_reponse(exception)
+    render json: ErrorSerializer.new(exception).bad_request, status: 404
+  end
 end
