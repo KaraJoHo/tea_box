694e48938e3c0865049331df6e22ab4df43f02ba
KaraJH <karas.jones@yahoo.com>
2023-06-07 10:58:30 -0600

    feat: Customer resources with subscriptions route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :subscriptions, only: [:create]
+      resources :customers do 
+        resources :subscriptions, only: [:index]
+      end
     end
   end
 end
