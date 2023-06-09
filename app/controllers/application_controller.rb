5ea867f459f0584da66c0518c3f941f7bf02cd4d
KaraJH <karas.jones@yahoo.com>
2023-06-09 10:15:01 -0600

    Fix create and clean up error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_reponse
   rescue_from ActionController::ParameterMissing, with: :missing_parameter_response
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_reponse
-
-  def render_not_found_response(exception)
-    render json: ErrorSerializer.new(exception).bad_request, status: 404
-  end
 
   def missing_parameter_response(exception)
     render json: ErrorSerializer.new(exception).bad_request, status: 400
